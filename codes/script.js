838c46057b74e04e5aa402ee9778c9b30a976f0c
Leonardo Weiss <lsweissleo@gmail.com>
2022-07-03 20:21:18 -0300

    Adding events and function to reset time

--- codes/script.js
+++ codes/script.js
@@ -4,33 +4,48 @@ var fSec = 0;
 var minutes = document.querySelector('#minutes');
 var seconds = document.querySelector('#seconds');
 var fractionSec = document.querySelector('#fSec');
-var stopWatch = setInterval(() => {
-	if (fSec < 9) {
-		fractionSec.innerHTML = '0' + ++fSec;
-	} else {
-		fractionSec.innerHTML = ++fSec;
-	}
-	if (fSec > 99) {
-		fSec = 0;
-		if (sec < 9) {
-			seconds.innerHTML = '0' + ++sec;
-		} else {
-			seconds.innerHTML = ++sec;
-		}
-		if (sec > 59) {
-			seconds.innerHTML = '00';
-			sec = 0;
-			if (min < 9) {
-				minutes.innerHTML = '0' + ++min + ':';
-			} else {
-				minutes.innerHTML = ++min + ':';
-			}
-		}
-	}
-}, 10);
 
-addEventListener('keypress', (e) =>{
-	if(e.which == 13){
+var stopWatch;
+
+addEventListener('keypress', (enter) =>{
+	if(enter.which == 13){
 		clearInterval(stopWatch);
 	  }
-}, false)
+}, false);
+
+addEventListener('keypress', (p) =>{
+	if(p.which == 112){
+		clearInterval(stopWatch);
+		min = 0; sec = 0; fSec = 0;
+		fractionSec.innerHTML = '00'; seconds.innerHTML = '00'; minutes.innerHTML = '00';
+	  }
+}, false);
+
+addEventListener('keypress', (space) =>{
+	if(space.which == 32){
+		stopWatch = setInterval(() => {
+			if (fSec < 9) {
+				fractionSec.innerHTML = '0' + ++fSec;
+			} else {
+				fractionSec.innerHTML = ++fSec;
+			}
+			if (fSec > 99) {
+				fSec = 0;
+				if (sec < 9) {
+					seconds.innerHTML = '0' + ++sec;
+				} else {
+					seconds.innerHTML = ++sec;
+				}
+				if (sec > 59) {
+					seconds.innerHTML = '00';
+					sec = 0;
+					if (min < 9) {
+						minutes.innerHTML = '0' + ++min + ':';
+					} else {
+						minutes.innerHTML = ++min + ':';
+					}
+				}
+			}
+		}, 10);
+	  }
+}, false);
